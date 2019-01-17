6fe454447e7862addd9e5c416ef80a649b2534d3
Danish Faisal <43044452+danish-faisal@users.noreply.github.com>
2019-01-17 19:54:25 +0530

    Add files via upload

---
+++ assgn4/ComplexNo&Employee.py
@@ -0,0 +1,39 @@
+#Complex Number Class
+class CompNo:
+    def __init__(self,real,imag):
+        self.real=real
+        self.imag=imag
+    def add(self,cmpno):
+        self.real=self.real+cmpno.real
+        self.imag=self.imag+cmpno.imag
+    def sub(self,cmpno):
+        self.real=self.real-cmpno.real
+        self.imag=self.imag-cmpno.imag
+    def __str__(self):
+        if self.imag < 0:
+            return (str(self.real)+str(self.imag)+"i")
+        else:
+            return (str(self.real)+"+"+str(self.imag)+"i")
+
+#Employee Class
+class Employee:
+    def __init__(self,name,salary,isSen):
+        self.name=name
+        self.salary=salary
+        self.isSen=isSen
+    def isAsen(self):
+        if(self.isSen):
+            print("I am a senior employee")
+        else:
+            print("I am a junior employee")
+
+c1=CompNo(2,4)
+c2=CompNo(4,8)
+c1.add(c2)
+print(c1)
+c2.sub(c1)
+print(c2)
+e1=Employee("A",2000,True)
+e2=Employee("B",3000,False)
+e1.isAsen()
+e2.isAsen()
