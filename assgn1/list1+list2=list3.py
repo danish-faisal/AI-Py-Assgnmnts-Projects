0411a1b11ddf929dbd4eb163ccc12f88973a0855
Danish Faisal <43044452+danish-faisal@users.noreply.github.com>
2019-01-17 19:50:40 +0530

    Add files via upload

---
+++ assgn1/list1+list2=list3.py
@@ -0,0 +1,4 @@
+lst1=input("Enter 3 elements for list1:").split(' ')
+lst2=input("Enter 3 elements for list2:").split(' ')
+lst3=[float(lst1[0])+float(lst2[0]),float(lst1[1])+float(lst2[1]),float(lst1[2])+float(lst2[2])]
+print(lst3)
